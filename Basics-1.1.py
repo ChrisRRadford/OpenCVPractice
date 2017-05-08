adc387e38cf34e1cb6fa29766f167658ba51d98d
Chris Radford <chrisradford@Chriss-iMac.local>
2017-05-08 14:39:37 -0400

    updated file location of image

--- Basics-1.1.py
+++ Basics-1.1.py
@@ -3,7 +3,7 @@ import cv2
 
 def Main():
 	#img = cv2.resize(cv2.imread('/Users/chrisradford/Documents/Research/avanetics-5775d1/_DSC0061.jpg',1),(1200,900))
-	img = cv2.resize(cv2.imread('/Users/chrisradford/Documents/Research/ImagesToPass/StitchTest/_001.jpg',1),(1200,900))	
+	img = cv2.resize(cv2.imread('/Users/chrisradford/Documents/Research/ImagesToPass/image1.jpg',1),(1200,900))	
 	cv2.imshow('image',img)
 	cv2.waitKey(0)
 	#cv2.destroyNamedWindow('images')
